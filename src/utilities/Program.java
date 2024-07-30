6cf478b3cd04d1ae2eb090bfd64e9a4b42ec58c9
udivak <udivak9@gmail.com>
2024-07-30 13:23:07 +0300

    finished

--- src/utilities/Program.java
+++ src/utilities/Program.java
@@ -1,11 +1,4 @@
 package utilities;
-import game.GameEngine;
-import game.arena.WinterArena;
-import game.competition.SkiCompetition;
-import game.competition.WinterCompetition;
-import game.entities.sportsman.Skier;
-import game.entities.sportsman.Snowboarder;
-import game.enums.*;
 
 /**
  * @author Itzhak Eretz Kdosha
@@ -13,44 +6,6 @@ import game.enums.*;
  */
 public class Program {
 	public static void main(String[] args) {
-		/*Skier skier1 = new Skier("sk1",23, Gender.MALE, 4.5,60, Discipline.DOWNHILL);
-		Skier skier2 = new Skier("sk2",25, Gender.MALE, 5.0,50, Discipline.DOWNHILL);
-		Skier skier3 = new Skier("sk3",23, Gender.FEMALE, 3.5,45, Discipline.GIANT_SLALOM);
-		Snowboarder jonsnowboarder = new Snowboarder("jon",25, Gender.FEMALE, 6.5,50, Discipline.DOWNHILL);
-		Skier skier4 = new Skier("sk4",29, Gender.MALE, 4.6,75, Discipline.DOWNHILL);
-		Skier skier5 = new Skier("sk5",50, Gender.MALE, 3.3,80, Discipline.DOWNHILL);
-
-		WinterArena arena = new WinterArena(1000,SnowSurface.CRUD,WeatherCondition.SUNNY);
-
-		SkiCompetition competition = new SkiCompetition(arena,3,Discipline.DOWNHILL, League.ADULT, Gender.MALE);
-		competition.addCompetitor(skier1);
-		competition.addCompetitor(skier2);
-		System.out.println("--------------- Exception example 1 ---------------");
-		try{
-			competition.addCompetitor(skier3);
-		}
-		catch (IllegalArgumentException e){
-			e.printStackTrace(System.out);
-		}
-		System.out.println("--------------- Exception example 2 ---------------");
-		try{
-			competition.addCompetitor(jonsnowboarder);
-		}
-		catch (IllegalArgumentException e){
-			e.printStackTrace(System.out);
-		}
-		System.out.println("--------------- Exception example 3 ---------------");
-
-		competition.addCompetitor(skier4);
-
-		try{
-			competition.addCompetitor(skier5);
-		}
-		catch (IllegalStateException e){
-			e.printStackTrace(System.out);
-		}
-		System.out.println("--------------- COMPETE ---------------");
-		GameEngine.getInstance().startRace(competition);*/
 		CompetitionGUI gui = new CompetitionGUI();
 		gui.setVisible(true);
 	}
